01b208d646ceea4420b3a49573b098b55df0689c
spencehood <spho6674@colorado.edu>
2016-03-27 13:18:51 -0600

    Added all files

---
+++ js/main.js
@@ -0,0 +1,7 @@
+$('document').ready(function() {
+
+	var windowHeight = $(window).height();
+	var windowMiddle = (windowHeight / 2) - 30;
+	$('#loadContain').css('margin-top', windowMiddle + 'px');
+	
+});
